8bb6a4bb2773a015f442eb92e9be2f64c02556d9
AlejandroJimenez16 <alejanjiga@gmail.com>
2026-02-20 20:29:19 +0100

    Movido destructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 12:49:09 by alejandj          #+#    #+#             */
-/*   Updated: 2026/02/18 13:51:11 by alejandj         ###   ########.fr       */
+/*   Updated: 2026/02/20 20:24:56 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ Zombie::Zombie(std::string name)
     this->name = name;
 }
 
-void Zombie::announce()
-{
-    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
 Zombie::~Zombie()
 {
     std::cout << "\033[1;31m" << name << " [DEAD] ☠️" << "\033[0m" << std::endl;
+}
+
+void Zombie::announce()
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 12:49:09 by alejandj          #+#    #+#             */
-/*   Updated: 2026/02/19 12:28:11 by alejandj         ###   ########.fr       */
+/*   Updated: 2026/02/20 20:25:19 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ Zombie::Zombie(std::string name)
     this->name = name;
 }
 
+Zombie::~Zombie()
+{
+    std::cout << "\033[1;31m" << name << " [DEAD] ☠️" << "\033[0m" << std::endl;
+}
+
 void Zombie::announce()
 {
     std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
@@ -27,9 +32,4 @@ void Zombie::announce()
 void Zombie::setName(std::string name)
 {
     this->name = name;
-}
-
-Zombie::~Zombie()
-{
-    std::cout << "\033[1;31m" << name << " [DEAD] ☠️" << "\033[0m" << std::endl;
 }
