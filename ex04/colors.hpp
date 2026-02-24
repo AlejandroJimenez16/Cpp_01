68e07f6d3f5e7899bef9ae8570722f122a96270c
AlejandroJimenez16 <alejanjiga@gmail.com>
2026-02-24 15:19:32 +0100

    ex04 hecho, faltan las pruebas

---
+++ ex04/colors.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 12:45:11 by alejandj          #+#    #+#             */
+/*   Updated: 2026/02/23 12:45:18 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+#define RESET   "\033[0m"
+#define BOLD	"\033[1m"
+
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+#endif
---
+++ ex04/main.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 12:40:03 by alejandj          #+#    #+#             */
+/*   Updated: 2026/02/24 15:06:52 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "colors.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << BOLD << RED << "Error: Invalid arguments" << RESET << std::endl;
+		std::cerr << BOLD << YELLOW << "Args format: ./sedIsForLosers <filename> <s1> <s2>" << RESET << std::endl;
+		return (1);
+	}
+	
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	
+	if (filename.empty() || s1.empty() || s2.empty())
+	{
+		std::cerr << BOLD << RED << "Error: Arguments cannot be empty" << RESET << std::endl;
+		return (1);
+	}
+
+	std::ifstream infile(filename);
+	if (!infile.is_open())
+	{
+		std::cerr << BOLD << RED << "Error: Cannot open file '" << filename << "'" << RESET << std::endl;
+		return (1);
+	}
+
+	std::string outfileName = filename + ".replace";
+	std::ofstream outfile(outfileName);
+	if (!outfile.is_open())
+	{
+		std::cerr << BOLD << RED << "Error: Cannot create file '" << filename << "'" << RESET << std::endl;
+		return (1);
+	}
+
+	std::string line;
+	while (getline(infile, line))
+	{
+		int pos = 0;
+		std::string newline;
+		size_t find_pos = line.find(s1);
+		while (find_pos != std::string::npos)
+		{
+			newline.append(line.substr(pos, find_pos - pos));
+			newline.append(s2);
+			pos = find_pos + s1.length();
+			find_pos = line.find(s1, pos);
+		}
+		newline.append(line.substr(pos));
+		outfile << newline << std::endl;
+	}
+
+	infile.close();
+	outfile.close();
+	return (0);
+}
